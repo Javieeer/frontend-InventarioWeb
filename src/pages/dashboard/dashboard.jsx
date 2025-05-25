c42367cc4070e1bba87efb368e0b58d17d0f2f07
Javier Zapata <zapatajavier34@gmail.com>
2025-05-25 15:59:27 -0500

    Correcciones de formato

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,5 +1,5 @@
 import { Typography, CssBaseline, Box } from "@mui/material";
-import { useAuth } from "../../context/AuthContext";
+import { useAuth } from "../../context/authContext";
 import { styles } from "../../styles/dashboard"; // importa tus estilos como objeto
 import Saludo from "../../components/saludo";
 import MenuLateral from "../../components/menuLateral";
