1c49cf4f5874f2fd5202cce570f3397b61e08c72
Javier Zapata <zapatajavier34@gmail.com>
2025-05-25 15:43:08 -0500

    Despliegue automatico

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/NOMBRE_DEL_REPO/',
+    base: 'frontend-InventarioWeb',
     plugins: [react()],
 })
